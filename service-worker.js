98dbfff2eb69c29fa193980bddf493e353eabb24
Leyasri <168668144+Leyasri@users.noreply.github.com>
2025-07-25 15:49:14 +0530

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,40 @@
-self.addEventListener('install', function(event) {
+const CACHE_NAME = "planpal-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "icon.png"
+];
+
+// Install
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open('planpal-cache-v1').then(function(cache) {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icon.png'
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("🗂 Caching app shell");
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            console.log("🧹 Clearing old cache");
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
